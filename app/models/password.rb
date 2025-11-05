dfab9664fecf17d3bb18b09be1d1aa563abdcc77
AlbertoJTD <albertojtd@outlook.com>
2025-11-05 05:50:22 -0600

    Encrypts password and username

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,4 +1,7 @@
 class Password < ApplicationRecord
   has_many :user_passwords, dependent: :destroy
   has_many :users, through: :user_passwords, dependent: :destroy
+
+  encrypts :username, deterministic: true # Allows for searching by username
+  encrypts :password
 end
