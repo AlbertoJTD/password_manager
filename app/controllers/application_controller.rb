18d4ea9fd3cd8ab4e034f1f54e3890325ed65512
AlbertoJTD <albertojtd@outlook.com>
2025-11-08 15:47:33 -0600

    Add concern for language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
+  include Language
 
   def current_user_password
     @current_user_password ||= current_user.user_passwords.find_by(password: @password)
   end
 
   helper_method :current_user_password
-
-  private
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
 end
---
+++ app/controllers/concerns/language.rb
@@ -0,0 +1,17 @@
+module Language
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_locale
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+end
