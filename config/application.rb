48af3a493414251dce74470628c99b22a957a639
AlbertoJTD <albertojtd@outlook.com>
2025-11-08 11:12:40 -0600

    Setup stimulus js for view_component

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module PasswordManager
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    initializer 'app_assets', after: 'importmat.assets' do
+      Rails.application.config.assets.paths << Rails.root.join('app')
+    end
+
+    config.importmap.cache_sweepers << Rails.root.join('app/components')
   end
 end
