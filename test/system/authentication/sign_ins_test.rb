3b09c45cb2217289e7e126db1e8fd97f00f3ca90
AlbertoJTD <albertojtd@outlook.com>
2025-12-24 14:10:55 -0600

    Add system test for login with spanish locale

--- test/system/authentication/sign_ins_test.rb
+++ test/system/authentication/sign_ins_test.rb
@@ -6,7 +6,7 @@ module Authentication
       @user = users(:joe_doe)
     end
 
-    test 'user logs in' do
+    test 'user logs in (English)' do
       visit new_user_session_path
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: 'password'
@@ -16,7 +16,7 @@ module Authentication
       assert_selector 'h1', text: 'All Passwords'
     end
 
-    test 'user tries to log in with invalid credentials' do
+    test 'user tries to log in with invalid credentials (English)' do
       visit new_user_session_path(I18n.locale)
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: 'wrong_password'
@@ -24,6 +24,26 @@ module Authentication
 
       assert_current_path new_user_session_path(I18n.locale)
       assert_selector 'p.alert', text: 'Invalid Email or password.'
+    end
+
+    test 'user logs in (Spanish)' do
+      visit new_user_session_path(locale: :es)
+      fill_in 'Email', with: @user.email
+      fill_in 'Contraseña', with: 'password'
+      click_button 'Iniciar sesión'
+
+      assert_current_path root_path(locale: :es)
+      assert_selector 'h1', text: 'Todas las contraseñas'
+    end
+
+    test 'user tries to log in with invalid credentials (Spanish)' do
+      visit new_user_session_path(locale: :es)
+      fill_in 'Email', with: @user.email
+      fill_in 'Contraseña', with: 'wrong_password'
+      click_button 'Iniciar sesión'
+
+      assert_current_path new_user_session_path(locale: :es)
+      assert_selector 'p.alert', text: 'Email y/o contraseña inválidos.'
     end
   end
 end
